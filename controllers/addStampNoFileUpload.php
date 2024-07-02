026e5a619626fe18a1e7de21c335e0ab995b6d1e
chun <quelquefoi@gmail.com>
2024-07-02 19:11:05 +0800

    addStamp upload function ongoing

--- controllers/addStampNoFileUpload.php
+++ controllers/addStampNoFileUpload.php
@@ -1,6 +1,6 @@
 <?php 
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: multipart/form-data; charset=UTF-8");
+header("Content-Type: application/json; charset=UTF-8");
 
 require_once("../controllers/connections/conn_db.php");
 
@@ -10,19 +10,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $price = !empty($input['price']) ? $input['price'] : NULL;
     $year = !empty($input['year']) ? $input['year'] : NULL;
     $unit = !empty($input['unit']) ? $input['unit'] : NULL;
+    $image = !empty($input['image']) ? $input['image'] : NULL;
     $remark = !empty($input['remark']) ? $input['remark'] : NULL;
-
-    //$image = !empty($input['image']) ? $input['image'] : NULL;
-
-    $imageName = NULL;
-    $sqlFindLastFile = "SELECT MAX(img) FROM stamp";
-    $queryFindLastFile = $link->query($sqlFindLastFile);
-    if ($queryFindLastFile) {
-        $lastFile = $queryFindLastFile->fetch();
-        $lastFileName = intval(pathinfo($lastFile[0], PATHINFO_FILENAME)) + 1;
-        $fileExtension = pathinfo($_FILES['image'], PATHINFO_EXTENSION);
-        $fileName = $lastFileName. '.'. $fileExtension;
-    }
 
     $sql = sprintf("INSERT INTO stamp (country_id, price, unit, year, img, remark) VALUES (%d, %d, '%s', '%s', '%s', '%s')", $country, $price, $unit, $year, $image, $remark);
     $query = $link->query($sql);
