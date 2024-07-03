0d60b4ecc76063d23cc27c41fd1aac0dac25037e
chun <quelquefoi@gmail.com>
2024-07-03 16:49:16 +0800

    image upload updated

---
+++ controllers/addStamp0702.php
@@ -0,0 +1,63 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: multipart/form-data; charset=UTF-8");
+
+require_once("../controllers/connections/conn_db.php");
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // $input = json_decode(file_get_contents('php://input'), true);
+    // $country = !empty($input['country']) ? $input['country'] : NULL;
+    // $price = !empty($input['price']) ? $input['price'] : NULL;
+    // $year = !empty($input['year']) ? $input['year'] : NULL;
+    // $unit = !empty($input['unit']) ? $input['unit'] : NULL;
+    // $remark = !empty($input['remark']) ? $input['remark'] : NULL;
+
+    $country = ($_POST['country']) ? $_POST['country'] : NULL;
+    $price = ($_POST['price']) ? $_POST['price'] : NULL;
+    $year = ($_POST['year']) ? $_POST['year'] : NULL;
+    $unit = ($_POST['unit']) ? $_POST['unit'] : NULL;
+    $remark = ($_POST['remark']) ? $_POST['remark'] : NULL;
+
+    $imageName = NULL;
+    $sqlFindLastFile = "SELECT MAX(img) FROM stamp";
+    $queryFindLastFile = $link->query($sqlFindLastFile);
+
+    if ($queryFindLastFile) {
+        $lastFile = $queryFindLastFile->fetch();
+        $lastFileNumber = intval(pathinfo($lastFile[0], PATHINFO_FILENAME)) + 1;
+        $lastFileName = str_pad($lastFileNumber,6, '0', STR_PAD_LEFT);
+        $fileExtension = strrchr($_FILES['image']['name'],'.');
+        $fileName = $lastFileName. $fileExtension;
+        // echo $fileName;
+        move_uploaded_file($_FILES['image']['tmp_name'], '../images/stamps/'.$fileName);
+    }
+
+
+    $sql = sprintf("INSERT INTO stamp (country_id, price, unit, year, img, remark) VALUES (%d, %d, '%s', '%s', '%s', '%s')", $country, $price, $unit, $year, $fileName, $remark);
+    $query = $link->query($sql);
+    if ($query) {
+        $sqlUpdate = sprintf("SELECT * FROM stamp WHERE country_id = %d AND year = %s AND price = %d", $country, $year, $price);
+        $queryUpdate = $link->query($sqlUpdate);
+        $stamps = $queryUpdate->fetchAll();
+        if ($stamps) {
+            $data = array(
+                'success' => true,
+                'message' => 'stamp added',
+                'stampData' => $stamps
+            );
+        } else {
+            $data = array(
+                'success' => false,
+                'message' => 'stamp added failed'
+            );
+        }
+    } else {
+        $data = array(
+            'success' => false,
+            'message' => 'stamp added failed'
+        );
+    }
+}
+echo json_encode($data, JSON_UNESCAPED_UNICODE);
+
+?>
